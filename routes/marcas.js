ca1eea1d1f68cc51330916c19d87cae04cfe9cc0
cleissongomes <cleissongomes777@gmail.com>
2024-04-03 10:28:30 -0300

    feat(routes): add new GET route to marcas.js

--- routes/marcas.js
+++ routes/marcas.js
@@ -42,4 +42,28 @@ router.get('/menosModelos', async (req, res) => {
   }
 });
 
+router.get('/listaMaisModelos/:id', async (req, res) => {
+  try {
+    let id = req.params.id;
+    const data = JSON.parse(await readFile('car-list.json'));
+
+    const contadorModelosPorMarca = {};
+    data.forEach(brand => {
+      contadorModelosPorMarca[brand.brand] = brand.models.length;
+    });
+
+    const marcasOrdenadas = Object.entries(contadorModelosPorMarca)
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, id);
+
+    const resultado = marcasOrdenadas.map(
+      ([marca, quantidade]) => `${marca} - ${quantidade}`
+    );
+
+    res.send(resultado);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 export default router;
